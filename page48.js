7b816ad1353db577bd2653ed78ab67c62872e13e
ilansav25 <ilan84988@gmail.com>
2024-03-18 21:38:37 +0200

    homw work updated assingments

--- page48.js
+++ page48.js
@@ -17,3 +17,66 @@ var results = [
 ];
 var points = calculatePoints(results);
 console.log("Points earned by Maccabi Tel Aviv:", points);
+
+//ex 2 
+function calculateAverage(data) {
+    var sum = 0;
+    var count = 0;
+
+    for (var i = 0; i < data.length; i++) {
+        if (data[i] === 0) {
+            break;
+        }
+        sum += data[i];
+        count++;
+    }
+
+    if (count > 0) {
+        return sum / count;
+    } else {
+        return 0;
+    }
+}
+
+var data = [9,0,7,3]; 
+var average = calculateAverage(data);
+console.log("The Average of the data:", average);
+
+//ex 3 
+
+function calculateTotalCost(checksRequested, totalAmountWithoutFees) {
+    var totalCost = 0;
+    var totalFees = 0;
+    var additionalFeesCounter = 0;
+
+    for (var i = 0; i < checksRequested; i++) {
+        var baseFee = 10;
+
+        var additionalFee = 0;
+        if (totalAmountWithoutFees > 0) {
+            if (totalAmountWithoutFees <= 500) {
+                additionalFee = 1;
+            } else {
+                additionalFee = 0.5; 
+            }
+        }
+
+        totalFees += baseFee + additionalFee;
+        additionalFeesCounter++;
+
+        totalAmountWithoutFees -= 100;
+       
+        totalCost += baseFee + additionalFee;
+    }
+
+    
+    console.log("Number of checks requested:", checksRequested);
+    console.log("Total amount of checks without fees:", totalAmountWithoutFees);
+    console.log("Total amount of fees:", totalFees.toFixed(2));
+    console.log("Total to be paid (cost + fees):", (totalCost + totalFees).toFixed(2));
+}
+
+
+var checksRequested = prompt("Enter your requested amout:");
+var totalAmountWithoutFees = prompt("Your total amount is:");
+calculateTotalCost(checksRequested, totalAmountWithoutFees);
